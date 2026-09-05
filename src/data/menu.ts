c92b4dfa899833d49978e469312e33bcce29fdba
dnlfrnk00 <frank.dan@outlook.de>
2026-09-05 08:51:14 +0200

    Obstteller entfernt.

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -185,7 +185,6 @@ export const menuData: MainCategory[] = [
                 imgSrc: "/Images/SnacksNFoodThumbnail.png",
                 subheading: "",
                 items: [
-                    { name: "Obstteller", price: "9.50", zutaten: "", zusatzstoffe: "", inhalt: "", alk: "" },
                     { name: "Pizza Sucuk", price: "10.50", zutaten: "", zusatzstoffe: "2, 9, 11, 12, 15", inhalt: "", alk: "" },
                     { name: "Pizza Margherita", price: "9.50", zutaten: "", zusatzstoffe: "2, 9, 11, 12, 15", inhalt: "", alk: "" },
                     { name: "Sucuk Baguette", price: "9.00", zutaten: "Sucuk, Gouda, Spezial Sauce", zusatzstoffe: "2, 9, 11, 12, 15", inhalt: "Mit Cocktailsoße-Dip", alk: "" },

dc19278f8708967b2b5e96186ae68078c1f6ecca
dnlfrnk00 <frank.dan@outlook.de>
2026-09-10 10:16:53 +0200

    Neue Darkblend-Sorte hinzugefügt.

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -15,6 +15,7 @@ export const menuData: MainCategory[] = [
                     { label: "Eisschlauch", price: "2.00" },
                 ],
                 items: [
+                    { name: "Cinnamon Roll (Zimtschnecke)", price: "18.00", zutaten: "", zusatzstoffe: "", inhalt: "", alk: "Darkblend - extra stark" },
                     { name: "Caribbean Rum", price: "18.00", zutaten: "", zusatzstoffe: "", inhalt: "", alk: "Darkblend - extra stark" },
                     { name: "Baileys Irish Cream", price: "18.00", zutaten: "", zusatzstoffe: "", inhalt: "", alk: "Darkblend - extra stark" },
                     { name: "Jungle Jus", price: "18.00", zutaten: "", zusatzstoffe: "", inhalt: "", alk: "Darkblend - extra stark" },
